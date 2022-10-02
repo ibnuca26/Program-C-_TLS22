264dbfbde0287c36385b6f8edc01e3a4942e254b
ibnuca26 <114848423+ibnuca26@users.noreply.github.com>
2022-10-02 13:10:43 +0700

    Add files via upload

---
+++ Program Penyewaan Gedung Olahraga.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+struct booking  {
+    char nama[10][10], tanggal[10][10];
+    int jumlah, lama[10], total, menu;
+      }book;
+      
+      void header(){
+          cout<<"================================="<<endl;
+          cout<<"PROGRAM PENYEWAAN GEDUNG OLAHRAGA"<<endl;
+          cout<<"================================="<<endl;
+      }
+
+int main(){
+    header();
+    cout<<"Masukan Jumlah pemboking : ";
+    cin>>book.jumlah;
+    for(int i=0;i<book.jumlah;i++){
+        cout<<endl;
+        cout<<"Masukan nama : ";
+        cin>>book.nama[i];
+        cout<<"Masukan durasi penyewaan :";
+        cin>>book.lama[i];
+        cout<<"Masukan tanggal penyewaan (dd/mm/yy): ";
+        cin>>book.tanggal[i];
+    }
+    cout<<endl;
+    header();
+    cout<<"#1 Lihat daftar pembooking"<<endl;
+    cout<<"#2 Keluar"<<endl;
+    cout<<"Pilih menu : ";
+    cin>>book.menu;
+    if(book.menu == 1){
+        for(int j=0; j<book.jumlah;j++){
+            cout<<endl;;
+            cout<<"Nama pembooking : "<<book.nama[j]<<endl;
+            cout<<"Durasi penyewaan : "<<book.lama[j]<<" jam"<<endl;
+            cout<<"Tanggal sewa : "<<book.tanggal[j]<<endl;
+            book.total=100000*book.lama[j];
+            cout<<"Biaya sewa : "<<book.total;
+            
+        }
+    }
+    else if(book.menu == 2){
+        exit(0);
+    }
+    else{
+        cout<<"Menu tidak ada!";
+    }
+}
